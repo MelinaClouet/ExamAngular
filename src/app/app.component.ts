7eaa6db4e0afcba252e13e2d1b7f56cb25e39e79
MelinaClouet <melina.clouet@my-digital-school.org>
2024-02-20 16:11:33 +0100

    [ADD] affichage tabeltte & mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormsModule} from "@angular/forms";
 import {Item} from "./interface/donnée.interface";
 import { ngxCsv } from 'ngx-csv/ngx-csv';
+import {CommonModule} from "@angular/common";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,12 +22,41 @@ export class AppComponent {
   produit= '';
   tabItems: Item[] = [];
   unite='';
+  view:string="";
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if(event.target.innerWidth<1180){
+      this.view="saisie";
+    }
+    else{
+      this.view="";
+    }
+  }
 
   constructor() {
     if (localStorage.getItem("inventaire")) {
       this.tabItems = JSON.parse(localStorage.getItem("inventaire") || '{}');
     }
 
+  }
+
+  ngAfterViewInit(){
+    if(window.innerWidth<1180){
+      this.view="saisie";
+    }
+    else{
+      this.view="";
+    }
+  }
+
+  changeViewInventaire(){
+    this.view="inventaire";
+  }
+
+  changeViewSaisie(){
+    this.view="saisie";
   }
 
 
@@ -91,6 +121,7 @@ export class AppComponent {
   allClear() {
     this.result = '';
     this.input = '';
+    this.produit = '';
   }
 
   calcAnswer() {
@@ -119,10 +150,6 @@ export class AppComponent {
 
   }
 
-  displayInventaire(){
-
-  }
-
   add(){
 
     switch (this.unite) {
@@ -144,15 +171,40 @@ export class AppComponent {
     }
 
     let object = {
+      id: Math.random().toString(36).substr(2, 9),
       produit: this.produit,
-      quantite: this.input,
+      quantite: this.result,
       unite: this.unite
     }
     this.tabItems.push(object);
     localStorage.setItem("inventaire", JSON.stringify(this.tabItems));
     console.log(this.tabItems);
+
   }
 
+  update(item: Item){
+    this.result=item.quantite;
+    this.produit=item.produit;
+    switch (item.unite) {
+      case "m2":
+        this.unite="1";
+        break;
+      case "ml":
+        this.unite="2";
+        break;
+      case "m3":
+        this.unite="3";
+        break;
+      case "U":
+        this.unite="4";
+        break;
+      case "l":
+        this.unite="5";
+        break;
+    }
+
+
+  }
   saveCSV(){
     var options = {
       fieldSeparator: ',',
@@ -171,6 +223,24 @@ export class AppComponent {
   }
 
   print(){
+    let htmlReport = "<table>";
+    htmlReport += "<tr>";
+    htmlReport += "<th>Produit</th>";
+    htmlReport += "<th>Quantité</th>";
+    htmlReport += "<th>Unité</th>";
+    htmlReport += "</tr>";
+    for (let i = 0; i < this.tabItems.length; i++) {
+      htmlReport += "<tr>";
+      htmlReport += "<td>" + this.tabItems[i].produit + "</td>";
+      htmlReport += "<td>" + this.tabItems[i].quantite + "</td>";
+      htmlReport += "<td>" + this.tabItems[i].unite + "</td>";
+      htmlReport += "</tr>";
+    }
+    htmlReport += "</table>";
+
+    window.document.write(htmlReport);
+    window.document.close();
+    window.print();
 
 
 
--- src/app/interface/donnée.interface.ts
+++ src/app/interface/donnée.interface.ts
@@ -1,4 +1,5 @@
 export interface Item {
+  id: string;
   produit: string;
   quantite: string;
   unite: string;
